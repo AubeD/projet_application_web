1fe8ffe40980510c18f3f27326d1f66c3283dea3
AubeD <o.bs@hotmail.fr>
2018-03-07 12:16:34 +0100

    rooms

--- server.js
+++ server.js
@@ -14,14 +14,26 @@ io.sockets.on('connection', function (socket, pseudo) {
     socket.on('nouveau_client', function(pseudo) {
         //pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
+        socket.room="room1";
+        socket.join("room1");
+        socket.emit('notification', 'you have connected to room1');
+        socket.broadcast.to('room1').emit('notification', pseudo+" s'est connecte");
 
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        socket.broadcast.to(socket.room).emit('message', {pseudo: socket.pseudo, message: message});
+    }); 
+
+    socket.on('rejoindre_room', function (room) {
+        socket.leave(socket.room);
+        socket.join(room);
+        socket.broadcast.to(socket.room).emit('notification', socket.pseudo+' has left this room');
+        socket.room=room;
+        socket.emit('notification', 'you have connected to '+socket.room);
+        socket.broadcast.to(socket.room).emit('notification', socket.pseudo+' a rejoint cette room');
     }); 
 
     socket.on('disconnect', function(){
